cab9e3b34e5bce341cfc6695ea2b7ac0ac11e487
roshaans2 <roshaansathya@gmail.com>
2023-10-11 19:02:23 +0530

    Creating a Basic Express Server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+const express = require("express")
+const dotenv = require("dotenv")
+const port = 5000
+
+const app = express()
+
+app.listen(port,()=>{
+    console.log(`Server started at port ${port}`)
+})
