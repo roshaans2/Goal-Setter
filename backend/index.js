d4588b114133d4632dc98a5a6ad20da0ee99245e
roshaans2 <roshaansathya@gmail.com>
2023-10-15 11:47:02 +0530

    Connecting to DB

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,16 @@
 const express = require("express")
+const colors = require("colors")
 const dotenv = require("dotenv").config()
 const port = process.env.PORT 
 const {errorHandler} = require("./middleware/errorMiddleware")
+const connectDB = require("./config/db")
+
+connectDB()
 
 const app = express()
 
 const goalRouter = require("./routes/goalRoutes")
+const { connect } = require("mongoose")
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
